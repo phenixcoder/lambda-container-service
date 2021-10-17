13186d28968ef5cb7240e52cff4c5ea99dc8ce91
Balwant <balwant.matharu@gmail.com>
2021-10-17 20:12:14 +1100

    fix(newman): fixed newman test failing due to tsc build config

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM public.ecr.aws/lambda/nodejs:14
 
 COPY .  ${LAMBDA_TASK_ROOT}
 
-RUN npm ci
+RUN npm ci --only=production
 
 # Set the CMD to your handler (could also be done as a parameter override outside of the Dockerfile)
 CMD [ "app/index.handler" ]  
--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: [
+    "**/__tests__/**/*.ts?(x)",
+    "**/?(*.)+(spec|test).ts?(x)"
+  ],
+  collectCoverage: true,
+  // collectCoverageFrom: [
+  //   "./app/**/*.ts",
+  //   "!./app/**/*.js",
+  //   "!./app/**/*.d.ts",
+  //   "!./app/**/*.(spec|test).ts"
+  // ]
 };
