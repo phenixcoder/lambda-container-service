03e3b368ee51ecb4cd217c717ceeab508a36ef07
Balwant Singh <2805177+phenixcoder@users.noreply.github.com>
2023-03-27 02:08:31 +1100

    Update setup script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-REPO=045615149555.dkr.ecr.ap-southeast-2.amazonaws.com/lambda-container-service
+SERVICE_NAME=$REPO_NAME
+REPO=$REPO_URI
 
-docker tag lambda-container-service:latest $REPO:$1
+docker tag $SERVICE_NAME:latest $REPO:$1
 docker push $REPO:$1
-docker tag lambda-container-service:latest $REPO:latest
+docker tag $SERVICE_NAME:latest $REPO:latest
 docker push $REPO:latest
